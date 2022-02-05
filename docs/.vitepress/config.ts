2172a4feccd7668f892977767a6880996922f4a5
tom huang <yellowrush888@gmail.com>
2022-02-05 15:03:16 +0900

    发布命令行

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
   title: '日本語の日常🍵',
   description: '就一帮对跟日语学习一毛钱关系都没有的人找了一位日语教师来整理核对出来的日语学习资料，顺便记录一下各种各样的日常生活',
   themeConfig: {
-    // Type is `DefaultTheme.Config`
+    nav: [
+      { text: '五十音', link: '/gojuon' },
+      { text: '词汇', link: '/word' },
+      { text: '语法', link: '/grammer' },
+      { text: '阅读', link: '/read' },
+      { text: '听力', link: '/listen' },
+    ],
   },
 });
---
+++ docs/index.md
@@ -0,0 +1,17 @@
+---
+title: 测试文章标题
+editLink: true
+sidebar: true
+home: true
+head:
+  - - meta
+    - name: description
+      content: hello
+  - - meta
+    - name: keywords
+      content: super duper SEO
+---
+
+# {{ $frontmatter.title }}
+
+测试文章内容 balabala
