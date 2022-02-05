34725c843a0d0d6f353df9dad106b402779c2444
tom huang <yellowrush888@gmail.com>
2022-02-05 13:04:30 +0900

    vitepress初始化

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  base: '/jbook/',
+  title: '日本語の日常🍵',
+  description: '就一帮对跟日语学习一毛钱关系都没有的人找了一位日语教师来整理核对出来的日语学习资料，顺便记录一下各种各样的日常生活',
+  themeConfig: {
+    // Type is `DefaultTheme.Config`
+  },
+});
